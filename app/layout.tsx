f96c69a48e5fac6da82ca3e90f228b381f2df05a
MayukhBanerjee <mayukh647@gmail.com>
2025-04-01 15:15:37 +0530

    ;dfkds;[

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] })
 
